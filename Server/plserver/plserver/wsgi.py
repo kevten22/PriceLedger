444241f339c3acb3c5aec30b6de6f6d2d50341dd
Kevin Tena <kevten09@gmail.com>
2019-01-19 20:32:12 -0600

    Changed name of server and created urlConf for prices

--- Server/plserver/plserver/wsgi.py
+++ Server/plserver/plserver/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for pricedata project.
+WSGI config for plserver project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pricedata.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'plserver.settings')
 
 application = get_wsgi_application()
