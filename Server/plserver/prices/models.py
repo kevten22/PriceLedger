eaec4ec3def75d1166392f1454ad56264ba2da14
Kevin Tena <kevten09@gmail.com>
2019-01-23 18:19:33 -0600

    Added .env file for secret key using python-decouple

--- Server/plserver/prices/models.py
+++ Server/plserver/prices/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+import uuid
 
 # Create your models here.
+class Item(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=64)
+    price = models.PositiveIntegerField()
+    purchased = models.DateTimeField(auto_now_add=True)
+    quantity = models.PositiveIntegerField()
