c9fec571b4ef3679c84e083547144d1e84364743
Kevin Tena <kevten09@gmail.com>
2019-01-30 23:27:13 -0600

    Progress made debuggin the user-detail viewset

--- Server/plserver/plserver/urls.py
+++ Server/plserver/plserver/urls.py
@@ -16,12 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
-from notes.api import ItemViewSet
+from prices.api import ItemViewSet
 
 router = routers.DefaultRouter()
 router.register(r'prices', ItemViewSet)
 
 urlpatterns = [
-    path('prices/', include('router.urls')),
+    path('api/', include(router.urls)),
     path('admin/', admin.site.urls),
 ]
--- Server/plserver/prices/api.py
+++ Server/plserver/prices/api.py
@@ -1,12 +1,30 @@
 from rest_framework import serializers, viewsets
 from .models import Item
+from django.contrib.auth.models import User
 
 class ItemSerializer(serializers.HyperlinkedModelSerializer):
 
     class Meta:
         model = Item
         fields = ('name', 'price', 'purchased', 'quantity', 'user')
+    
+    def create(self, validated_data):
+        user = self.context['request'].user
+        item = Item.objects.create(user=user, **validated_data)
+        return item
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name="prices:user-detail")
+
+    class Meta:
+        model = User
+        fields = ('url', 'username')
 
 class ItemViewSet(viewsets.ModelViewSet):
     serializer_class = ItemSerializer
     queryset = Item.objects.all()
+
+class UserViewSet(viewsets.ModelViewSet):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
